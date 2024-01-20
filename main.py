1cf192de14749160908ede6408d27a9050509b7c
Zaed Khan <zaedkhan2019@gmail.com>
2024-01-20 23:59:20 +0600

    Added default args and docstrings

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 ## Python uses an interpreter called CPython
 ## You can use .sort() to sort a list
 
-def get_todos(filepath):
+def get_todos(filepath='todos.txt'):
+    """ Read a text file and return the list of to-do items. """
     with open(filepath, 'r') as file:
         todos = file.readlines()
     return todos
 
-def write_todos(todos, filepath):
+def write_todos(todos, filepath='todos.txt'):
+    """ Write a list of to-do items to a text file. """
     with open(filepath, 'w') as file:
         file.writelines(todos)
 
